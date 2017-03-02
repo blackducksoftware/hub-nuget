9ec848d4aa1f4537870698382e10b65b6b94d29a
Jake Mathews <mathewsj2@wit.edu>
2017-03-02 07:26:10 -0500

    the class to handle processing solution files

---
+++ BuildBomTask/SolutionBuildBOMTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace Com.Blackducksoftware.Integration.Hub.Nuget
+{
+    public class SolutionBuildBOMTask : BuildBOMTask
+    {
+        public override bool Execute()
+        {
+            // for each project set the properties and call the base execute method.
+            return base.Execute();           
+        }
+    }
+}
