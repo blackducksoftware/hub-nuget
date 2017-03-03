ae2b87379ba7947065052beb598fb7e0d53ac2c4
Paulo Santos <psantos@blackducksoftware.com>
2017-03-03 07:11:06 -0500

    changes to iterate through each project

--- BuildBomTask/SolutionBuildBOMTask.cs
+++ BuildBomTask/SolutionBuildBOMTask.cs
@@ -1,14 +1,70 @@
 ﻿using System;
+using Microsoft.Build.Construction;
 using Microsoft.Build.Utilities;
 
 namespace Com.Blackducksoftware.Integration.Hub.Nuget
 {
     public class SolutionBuildBOMTask : BuildBOMTask
     {
+        [Required]
+        public string SolutionPath { get; set; }
+
         public override bool Execute()
         {
-            // for each project set the properties and call the base execute method.
-            return base.Execute();           
+            try
+            {
+                var solutionFile = SolutionFile.Parse(SolutionPath);
+                var projectList = solutionFile.ProjectsInOrder;
+
+                foreach(ProjectsInSolution project in projectList)
+                {
+                    HubProjectName=project.ProjectName;
+                    HubVersionName= GetProjectAssemblyVersion(project);
+                    PackagesConfigPath=project.AbsolutePath+"\packages.config";
+                    if(base.Execute())
+                    {
+                        Console.WriteLine("Generated Bdio file for project {0}",)project.ProjectName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Could not generate Bdio file for project {0}",)project.ProjectName);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                if(HubIgnoreFailure)
+                {
+                    Console.WriteLine("Error Occurred build Black Duck I/O file for solution: {0}",ex);
+                }
+                else
+                {
+                    throw new BlackDuckIntegrationException(ex);
+                }
+            }
+        }
+
+        public string GetProjectAssemblyVersion(ProjectsInSolution project)
+        {
+            string version = DateTime.Now().ToString("yyyy-MM-dd_HH-mm-ss");
+            string path = project.AbsolutePath+"\Properties\AssemblyInfo.cs";
+
+            if(File.Exists(path))
+            {
+                string[] contents = File.ReadAllLines(path);
+                var versionText = contents.Where( text => text.Contains("[assembly: AssemblyVersion"));
+                foreach(string text in versionText)
+                {
+                    int firstParen = text.IndexOf("(");
+                    int lastParen = text.LastIndexOf(")");
+                    // exclude the '(' and the " characters
+                    int start = firstParen + 2;
+                    // exclude the ')' and the " characters
+                    int end = lastParen - 2;
+                    version = item.Substring(start, end);
+                }
+            }
+            return version;
         }
     }
 }
