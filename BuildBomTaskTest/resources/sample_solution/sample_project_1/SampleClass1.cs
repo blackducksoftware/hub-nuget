dc5cf6e31671603b236946ed65e4d6c944e36cf4
Jake Mathews <mathewsj2@wit.edu>
2017-03-06 08:51:01 -0500

    add a sample solution for generating bdio

---
+++ BuildBomTaskTest/resources/sample_solution/sample_project_1/SampleClass1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Blackducksoftware.Integration.Hub.Nuget.Sample
+{
+    public class SampleClass1
+    {
+    }
+}
---
+++ BuildBomTaskTest/resources/sample_solution/sample_solution/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Com.Blackducksoftware.Integration.Hub.Nuget.Sample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Blackducksoftware.Integration.Hub.Nuget.Sample
+{
+    public class Class1
+    {
+        public Class1() : base()
+        {
+            SampleClass1 class1 = new SampleClass1();
+            SampleClass2 class2 = new SampleClass2();
+            Console.WriteLine("Created objects.");
+        }
+        
+        public static void Main(string[] args)
+        {
+            Class1 newinstance = new Class1();
+            Console.WriteLine("Sample main method executed.");
+        }
+    }
+    
+}
