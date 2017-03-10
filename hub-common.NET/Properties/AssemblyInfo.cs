53f6220f385c74a14b0696ff8ad16d5407281c96
psantos1113 <mathewsj2@wit.edu>
2017-03-10 12:46:19 -0500

    update to use nuget package for integration-bdio.NET

--- hub-common.NET/Properties/AssemblyInfo.cs
+++ hub-common.NET/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("hub-common.NET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for communicating with the Hub")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Black Duck Software")]
 [assembly: AssemblyProduct("hub-common.NET")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
