59b3e2eb6f0181ae235ed9447c6c9a8d6e1748e5
psantos1113 <psantos@blackducksoftware.com>
2017-03-13 14:32:48 -0400

    prepare a 0.0.4 for hub-common.NET

--- BuildBomTask/BlackDuckIntegrationException.cs
+++ BuildBomTask/BlackDuckIntegrationException.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Net.Http;
+using System.Runtime.Serialization;
 
 namespace Com.Blackducksoftware.Integration.Hub.Nuget
 {
     [Serializable]
     class BlackDuckIntegrationException : Exception
     {
-
         public BlackDuckIntegrationException(): base()
         {
-
         }
 
         public BlackDuckIntegrationException(string message) : base(message)
@@ -18,7 +17,14 @@ namespace Com.Blackducksoftware.Integration.Hub.Nuget
 
         public BlackDuckIntegrationException(HttpResponseMessage content) : base($"Response returned with: {content.ReasonPhrase}")
         {
-       
+        }
+
+        public BlackDuckIntegrationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public BlackDuckIntegrationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
         }
     }
 }
